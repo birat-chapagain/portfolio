59319bce165f072358a08cf43ec4421576d57216
Rohit Sah <sahrohit9586@gmail.com>
2022-07-20 23:51:06 +0530

    Converted Chakra to Tailwind

--- config.ts
+++ config.ts
@@ -1,3 +1,6 @@
+import { Theme } from "types";
+
+export const theme: Theme = "emerald";
 export const infomation = {
   name: "Rohit",
 };
--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-})
-module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'mdx'],
-})
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: false,
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "placeimg.com",
+      "picsum.photos",
+      "via.placeholder.com",
+      "images.pexels.com",
+      "m.media-amazon.com",
+      "tailwindui.com",
+    ],
+  },
+};
